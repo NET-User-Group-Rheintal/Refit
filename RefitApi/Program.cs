24731fc1f54038dacd863ed90d6d77c3687424b5
Pacman313313 <office@marcowagner.at>
2024-01-30 19:47:22 +0100

    chore: Use same Endpoint class as Refit ApiEndpoints.WeatherForecast.Get

--- RefitApi/Program.cs
+++ RefitApi/Program.cs
@@ -1,3 +1,5 @@
+using RefitAppShared;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,7 +23,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet(ApiEndpoints.WeatherForecast.Get, () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
